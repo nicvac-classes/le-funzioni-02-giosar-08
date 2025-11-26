9b2c8fd1e80d5064d9584c102cf3cbdba07fb1dd
giosar-08 <giorgio.saracino@marconibari.edu.it>
2025-11-26 16:15:42 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+  int n1, n2, n3, massimo;
+  cout<<"inserisci 3 valori"<<;
+  cin>>n1>>n2>>n3;
+  {
+    massimo (int n1, int, n2)
+    if (n1>n2)
+    {
+        massimo=n1
+    }
+        massimo=n2
+}   massimo (int massimo; int n3)
+    if (massimo<n3)
+    {
+        massimo=n3
+    }
+    cout<<"il numero massimo è " & massimo <<;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
